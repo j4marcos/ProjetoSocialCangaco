f40cf32d80cacd0925693fca2535642d2a52113e
j4marcos <joaomarcos2004.jonh@gmail.com>
2024-01-02 23:15:32 -0300

    remake

--- script.js
+++
@@ -1,8 +0,0 @@
-const navMenu = document.querySelector('.menu')
-const navBotao = document.querySelector('.navBotão')
-
-
-navBotao.addEventListener('click', (e) => {
-    navBotao.classList.toggle("ativo")
-    navMenu.classList.toggle("ativo")
-})
